0e042b3d6a0bea4b493dbd9cbd0c4b222e908dc3
nextgentech-in <setupatel01@gmail.com>
2026-09-07 12:38:32 +0530

    fix: import GoogleAuthProvider in AuthContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 import { auth, db, googleProvider } from '../firebase/firebaseConfig';
 import {
+  GoogleAuthProvider,
   onAuthStateChanged,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
